d9262a2c63291c03ea1a78107657e0bb72e47146
GitHub Action <action@github.com>
2025-03-04 04:23:42 +0000

    Deploy built site from source - 600cb36893f5104150c4c71c4078a4e8eec40a00

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 John's personal blog
 
-[![Build Status](https://travis-ci.org/iplanetcn/iplanetcn.github.io.svg?branch=master)](https://travis-ci.org/iplanetcn/iplanetcn.github.io)
+[![Build Status](https://github.com/iplanetcn/iplanetcn.github.io/actions/workflows/build.yaml/badge.svg)](https://github.com/iplanetcn/iplanetcn.github.io/actions/workflows/build.yaml)
 
 http://iplanetcn.github.io, powered by [hexo](https://hexo.io/) and based on the theme [NexT](https://github.com/iissnan/hexo-theme-next).
--- js/third-party/tags/mermaid.js
+++ js/third-party/tags/mermaid.js
@@ -1,25 +1,32 @@
 /* global NexT, CONFIG, mermaid */
 
 document.addEventListener('page:loaded', () => {
-  const mermaidElements = document.querySelectorAll('.mermaid');
+  const mermaidElements = document.querySelectorAll('pre > .mermaid');
   if (mermaidElements.length) {
     NexT.utils.getScript(CONFIG.mermaid.js, {
       condition: window.mermaid
     }).then(() => {
       mermaidElements.forEach(element => {
+        const box = document.createElement('div');
+        box.className = 'code-container';
         const newElement = document.createElement('div');
         newElement.innerHTML = element.innerHTML;
-        newElement.className = element.className;
-        element.parentNode.replaceChild(newElement, element);
+        newElement.className = 'mermaid';
+        box.appendChild(newElement);
+        if (CONFIG.copycode.enable) {
+          NexT.utils.registerCopyButton(box, box, element.textContent);
+        }
+        const parent = element.parentNode;
+        parent.parentNode.replaceChild(box, parent);
       });
-
-      mermaid.init({
-        theme    : CONFIG.mermaid.theme,
-        logLevel : 3,
-        flowchart: {curve: 'linear'},
-        gantt    : {axisFormat: '%m/%d/%Y'},
-        sequence : {actorMargin: 50}
-      }, '.mermaid');
+      mermaid.initialize({
+        theme    : CONFIG.darkmode && window.matchMedia('(prefers-color-scheme: dark)').matches ? CONFIG.mermaid.theme.dark : CONFIG.mermaid.theme.light,
+        logLevel : 4,
+        flowchart: { curve: 'linear' },
+        gantt    : { axisFormat: '%m/%d/%Y' },
+        sequence : { actorMargin: 50 }
+      });
+      mermaid.run();
     });
   }
 });
